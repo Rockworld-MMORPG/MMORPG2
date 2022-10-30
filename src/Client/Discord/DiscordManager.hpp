348bede7b1b52137cb18b30c6b02b927d3f0a893
AlexGarrity <garrity2799@gmail.com>
2022-10-30 21:07:11 +0000

    docs(Client): Document DiscordManager

--- src/Client/Discord/DiscordManager.hpp
+++ src/Client/Discord/DiscordManager.hpp
@@ -10,17 +10,55 @@ namespace discord
 namespace Client
 {
 
+	/**
+	 * \class DiscordManager DiscordManager.hpp "Discord/DiscordManager.hpp"
+	 * \brief Manages the state of the application's Discord integration
+	 */
 	class DiscordManager
 	{
 	public:
+		/**
+		 * \brief Get the Discord Manager singleton instance
+		 */
 		static auto get() -> DiscordManager&;
 
+		/**
+		 * \brief Update the Discord Manager
+		 *
+		 */
 		auto update() -> void;
+
+		/**
+		 * \brief Set the Discord status
+		 *
+		 * \param playerActivity A string detailing what the player is currently doing
+		 */
 		auto setStatus(const std::string& playerActivity) -> void;
+
+		/**
+		 * \brief Set the Discord status
+		 *
+		 * \param playerActivity A string detailing what the player is currently doing
+		 * \param partyActivity A string detailing what the party is currently doing
+		 * \param partySize The size of the party the player is in
+		 * \param maxPartySize The maximum size of the party the player is in
+		 */
 		auto setStatus(const std::string& playerActivity, const std::string& partyActivity, std::int32_t partySize, std::int32_t maxPartySize) -> void;
 
+		/**
+		 * \brief Set the Discord status asset
+		 *
+		 * \param largeImage A string containing the URL of the large image
+		 * \param largeText A string detailing what the large image is
+		 * \param smallImage A string containing the URL of the small image
+		 * \param smallText A string detailing what the small image is
+		 */
 		auto setAssets(std::string largeImage, std::string largeText, std::string smallImage, std::string smallText) -> void;
 
+		/**
+		 * \struct Assets DiscordManager.hpp "Discord/DiscordManager.hpp"
+		 * \brief Data representing the Discord Manager's status assets
+		 */
 		struct Assets
 		{
 			std::string largeImage;
@@ -30,6 +68,10 @@ namespace Client
 		};
 
 	private:
+		/**
+		 * \brief Construct a new Discord Manager object
+		 *
+		 */
 		DiscordManager();
 
 		std::unique_ptr<discord::Core> m_core;
